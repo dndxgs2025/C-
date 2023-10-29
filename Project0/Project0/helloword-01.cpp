131125a5764c8453a3b778edcb2beaa75199ff0a
dndxgs2025 <128151426+dndxgs2025@users.noreply.github.com>
2023-10-29 21:42:58 +0800

    Add files via upload

---
+++ Project0/Project0/helloword-01.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+void welcome();//要记得标注头文件
+
+int  main()
+{
+	
+	cout << "Hello word!" << endl;
+	//等同于scanf
+	
+	welcome();
+
+    cin.get();
+	cin.get();
+	return 0;
+}
---
+++ Project0/Project0/welcome.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+//定义一个欢迎函数
+void welcome() {
+
+	//用一个变量来保存键盘输入的信息
+	string name;
+	cin >> name;
+
+	//输出一句欢迎信息
+	cout << "Hello, " << name << endl;
+
+}
